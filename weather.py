bd3c95303f72f92d357a23313fde994cf6009a0b
JosephineVanpool <Josephine.Vanpool@gmail.com>
2021-07-07 15:06:54 -0500

    added 2 classes, one to return weather data (verified it works), and one to get the mode of the weather

--- weather.py
+++ weather.py
@@ -2,36 +2,32 @@
 import requests,json, pprint, datetime, pytz
 from statistics import mode
 
+def get_weather_data(latitude,longitude,api_key="09d4916d61723eb6f4bd9b01105317c9",base_url="https://api.openweathermap.org/data/2.5/onecall?lat="):
+    #pp =pprint.PrettyPrinter(indent =4)
 
-#pp =pprint.PrettyPrinter(indent =4)
+    #set latitude/longitude
+    lat= latitude
+    long= longitude
 
-#type your API KEY Here.
-api_key = "09d4916d61723eb6f4bd9b01105317c9"
-base_url = "https://api.openweathermap.org/data/2.5/onecall?lat="
+    #complete_url variable to store the complete_url address
+    complete_url = str(base_url + str(lat) + "&lon="+ str(long) +"&exclude={,minutely,hourly,alerts}&appid="+api_key)
 
-#taking input "city name" from user
-#city_name = input("Enter the city name: ")
+    #get response to make sure request is good
+    response = requests.get(complete_url)
+    code = response.status_code
+    print (code)
+    #if the reponse is ok, then get and return the json file, if not return an error message
+    if response.ok:
+        #json method of response object convert json format data into python format data
+        print ("Able to retreive file, response is code: " +str(code))
+        json_data = response.json()
+        return json_data
+        #print (pp.pprint(json_data))
+    else:
+        print ("Unable to retrieve file, response was code: "+str(code))
 
-#latitude/longitude
-lat= -19.5665
-long= 101.7068
-
-#complete_url variable to store the complete_url address
-complete_url = str(base_url + str(lat) + "&lon="+ str(long) +"&exclude={,minutely,hourly,alerts}&appid="+api_key)
-
-#get methods of requests module retruns respons object
-response = requests.get(complete_url)
-code = response.status_code
-
-#json method of response object convert json format data into python format data
-json_data = response.json()
-print (pp.pprint(json_data))
-print (code)
-
-daily_weather_list = []
-#Now x contains list of nested dictionaries
-#check the value of "cod" key is equal to "404", means city is found otherwise, city is not found
-if response.ok:
+def get_weather_mode():
+    daily_weather_list = []
     for index in range (0, 8):
         local_time = datetime.datetime.fromtimestamp( json_data['daily'][index]['dt'] , tz=pytz.timezone('America/Chicago'))
         str_time = local_time.strftime( '%Y-%m-%d %a' )
@@ -43,6 +39,13 @@ if response.ok:
     print (daily_weather_list)
     mode_daily_weather = mode (daily_weather_list)
     print (mode_daily_weather)
+   
 
-else:
-    print("City Not Found")                       
+
+if __name__ == "__main__":
+    latitude= -19.5665
+    longitude= 101.7068
+    get_weather_data (latitude, longitude)
+
+
+
