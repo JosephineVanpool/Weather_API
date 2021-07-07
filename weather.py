2af4997ea6ddd72f7c54a53750c6a02cc61617ff
JosephineVanpool <Josephine.Vanpool@gmail.com>
2021-07-07 15:32:26 -0500

    added try exception block to test for status

--- weather.py
+++ weather.py
@@ -12,21 +12,21 @@ def get_weather_data(latitude,longitude,api_key="09d4916d61723eb6f4bd9b01105317c
     #complete_url variable to store the complete_url address
     complete_url = str(base_url + str(lat) + "&lon="+ str(long) +"&exclude={,minutely,hourly,alerts}&appid="+api_key)
 
-    #get response to make sure request is good
-    response = requests.get(complete_url)
-    code = response.status_code
-    print (code)
     #if the reponse is ok, then get and return the json file, if not return an error message
-    if response.ok:
+    try: 
+        response = requests.get(complete_url)
+        response.raise_for_status()
+        
         #json method of response object convert json format data into python format data
-        print ("Able to retreive file, response is code: " +str(code))
         json_data = response.json()
-        return json_data
         #print (pp.pprint(json_data))
-    else:
-        print ("Unable to retrieve file, response was code: "+str(code))
+    except:
+        raise
 
-def get_weather_mode():
+    return json_data
+
+        
+def get_weather_mode(json_data):
     daily_weather_list = []
     for index in range (0, 8):
         local_time = datetime.datetime.fromtimestamp( json_data['daily'][index]['dt'] , tz=pytz.timezone('America/Chicago'))
@@ -46,6 +46,7 @@ if __name__ == "__main__":
     latitude= -19.5665
     longitude= 101.7068
     get_weather_data (latitude, longitude)
+
 
 
 
