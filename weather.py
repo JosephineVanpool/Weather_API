db3ae9f3a35f37b73572f397fb557938f7056ab3
JosephineVanpool <Josephine.Vanpool@gmail.com>
2022-01-25 18:10:44 -0600

    change print to be better looking

--- weather.py
+++ weather.py
@@ -26,11 +26,10 @@ def get_weather_data(latitude,longitude,api_key="09d4916d61723eb6f4bd9b01105317c
 
     return json_data
 
-        
+#get the daily weather        
 def get_weather_mode(location_name,json_data):
     daily_weather_list = []
     loc_name = location_name
-    print (loc_name)
     for index in range (0, 8):
         local_time = datetime.datetime.fromtimestamp( json_data['daily'][index]['dt'] , tz=pytz.timezone('America/Chicago'))
         #str_time = local_time.strftime( '%Y-%m-%d %a' )
@@ -39,14 +38,27 @@ def get_weather_mode(location_name,json_data):
         daily_weather_list.append(daily_weather)
         #print( f" Day [+{index}] {str_time} = {daily_weather} " )
 
-    print (daily_weather_list)
+    #print (daily_weather_list)
     mode_daily_weather = mode (daily_weather_list)
-    print (mode_daily_weather)
+    print (loc_name, "-",mode_daily_weather)
    
 
-
+#actually create the list
 if __name__ == "__main__":
-    location = {"Mexico Michcocan":(-19.5665,101.7068), "Mexico Sinaloa": (25.0, -107.499998)}
+    location = {
+    "Mexico Michcocan":(-19.5665,101.7068), 
+    "Mexico Sinaloa": (25.0, -107.499998), 
+    "USA New York": (40.730610,-73.935242),
+    "Canada Ontario": (50.000000, -85.000000),
+    "Mexico Puebla" :(19.05083313, -98.217332464),
+    "Mexico Jalisco": (20.33333, -103.66667),
+    "Mexico Nayarit": (21.739663708, -105.223665772),
+    "Mexico San Luis Potosi": (22.14982, -100.97916),
+    "Mexico Sonora": (29.66667, -110.5),
+    "Mexico Colima": (19.5075646364, -103.617130865),
+    "USA Michigan": (44.182205, -84.506836),
+    "Mexico Queretaro": (20.835996656, -99.85082993)
+    }
     for key in location:
         location_name = key
         latitude= location[key][0]
@@ -55,9 +67,6 @@ if __name__ == "__main__":
         json_data=get_weather_data (latitude, longitude)
         get_weather_mode(location_name,json_data)
 
-#next step is to determine how to break mode, or how to handle when there is no mode in the list
-#also need the complete list of locations and lat/long coordinates to add to the location list in the main function
-#will want to create a seperate dict with location name and the mode (or the list of daily weather if no mode exists)
 
 
 
